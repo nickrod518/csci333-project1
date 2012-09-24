d85f022ce2ee42068f0d80f25425989ed1e8bed7
Nick Rodriguez <nickrod518@gmail.com>
2012-09-24 17:16:28 -0400

    Implemented timing in main function

--- src/queue_tester.cpp
+++ src/queue_tester.cpp
@@ -2,73 +2,113 @@
 #include "LQueue/LQueue.h"
 #include "VQueue/VQueue.h"
 #include <iostream>
+#include <ctime>
 
 using std::cout;
 using std::endl;
 
 int main() {
+  int start, end, elapsed;
+  double seconds;
+
 
   // AQueue testing
-  cout << "begin AQueue testing" << endl;
+  start = clock();
+  cout << "Begin AQueue testing..." << endl;
 
-  AQueue* aQueue = new AQueue(5);
-  aQueue->enqueue(1);
-  aQueue->enqueue(2);
-  aQueue->enqueue(3);
-  aQueue->enqueue(4);
-  aQueue->enqueue(5);
-  cout<< "size " << aQueue->size() << endl;
+  AQueue* aQueue = new AQueue();
+  for (int i = 0; i < 1000; ++i)
+    aQueue->enqueue(i);
+  cout << "Size after enqueueing 1000: " << aQueue->size() << endl;
 
-  cout << aQueue->dequeue() << endl;
-  cout << aQueue->dequeue() << endl;
-  cout << aQueue->dequeue() << endl;
-  cout << aQueue->dequeue() << endl;
-  cout << aQueue->dequeue() << endl;
-  cout<< "size " << aQueue->size() << endl;
-  cout << "empty?" << aQueue->isEmpty() << endl;
+  for (int i = 0; i < 500; ++i)
+    aQueue->dequeue();
+  cout << "Size after dequeueing 500: " << aQueue->size() << endl;
+  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
+
+  for (int i = 0; i < 200; ++i)
+    aQueue->enqueue(i);
+  cout << "Size after enqueueing 200 more: " << aQueue->size() << endl;
+  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
+
+  for (int i = 0; i < 700; ++i)
+    aQueue->dequeue();
+  cout << "Size after dequeueing the remaining 700: " << aQueue->size() << endl;
+  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
 
   delete aQueue;
   
-  cout << "end AQueue testing" << endl;
+  cout << "...end AQueue testing." << endl;
+  end = clock();
+  elapsed = end - start;
+  seconds = (double) elapsed/CLOCKS_PER_SEC;
+  cout << "elapsed time: " << seconds << " seconds " << endl;
+  cout << endl;
 
 
   // LQueue testing
-  cout << "begin LQueue testing" << endl;
+  start = clock();
+  cout << "Begin LQueue testing..." << endl;
 
   LQueue lQueue = LQueue();
-  lQueue.enqueue(1);
-  cout << "empty?" << lQueue.isEmpty() << endl;
-  lQueue.enqueue(2);
-  lQueue.enqueue(3);
-  cout<< "size " << lQueue.size() << endl;
+  for (int i = 0; i < 1000; ++i)
+    lQueue.enqueue(i);
+  cout << "Size after enqueueing 1000: " << lQueue.size() << endl;
 
-  cout << lQueue.dequeue() << endl;
-  cout << lQueue.dequeue() << endl;
-  cout << lQueue.dequeue() << endl;
-  cout<< "size " << lQueue.size() << endl;
-  cout << "empty?" << lQueue.isEmpty() << endl;
+  for (int i = 0; i < 500; ++i)
+    lQueue.dequeue();
+  cout << "Size after dequeueing 500: " << lQueue.size() << endl;
+  cout << "Is it empty yet? " << lQueue.isEmpty() << endl;
 
-  cout << "end LQueue testing" << endl;
+  for (int i = 0; i < 200; ++i)
+    lQueue.enqueue(i);
+  cout << "Size after enqueueing 200 more: " << lQueue.size() << endl;
+  cout << "Is it empty yet? " << lQueue.isEmpty() << endl;
+
+  for (int i = 0; i < 700; ++i)
+    lQueue.dequeue();
+  cout << "Size after dequeueing the remaining 700: " << aQueue->size() << endl;
+  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
+
+  cout << "...end LQueue testing." << endl;
+  end = clock();
+  elapsed = end - start;
+  seconds = (double) elapsed/CLOCKS_PER_SEC;
+  cout << "elapsed time: " << seconds << " seconds " << endl;
+  cout << endl;
+
 
   // VQueue testing
-  cout << "begin VQueue testing" << endl;
+  start = clock();
+  cout << "Begin VQueue testing..." << endl;
 
   VQueue* vQueue = new VQueue();
-  vQueue->enqueue(1);
-  cout << "empty?" << vQueue->isEmpty() << endl;
-  vQueue->enqueue(2);
-  vQueue->enqueue(3);
-  cout<< "size " << vQueue->size() << endl;
+  for (int i = 0; i < 1000; ++i)
+    vQueue->enqueue(i);
+  cout << "Size after enqueueing 1000: " << vQueue->size() << endl;
 
-  cout << vQueue->dequeue() << endl;
-  cout << vQueue->dequeue() << endl;
-  cout << vQueue->dequeue() << endl;
-  cout<< "size " << vQueue->size() << endl;
-  cout << "empty?" << vQueue->isEmpty() << endl;
+  for (int i = 0; i < 500; ++i)
+    vQueue->dequeue();
+  cout << "Size after dequeueing 500: " << vQueue->size() << endl;
+  cout << "Is it empty yet? " << vQueue->isEmpty() << endl;
+
+  for (int i = 0; i < 200; ++i)
+    vQueue->enqueue(i);
+  cout << "Size after enqueueing 200 more: " << vQueue->size() << endl;
+  cout << "Is it empty yet? " << vQueue->isEmpty() << endl;
+
+  for (int i = 0; i < 700; ++i)
+    vQueue->dequeue();
+  cout << "Size after dequeueing the remaining 700: " << vQueue->size() << endl;
+  cout << "Is it empty yet? " << vQueue->isEmpty() << endl;
 
   delete vQueue;
   
-  cout << "end VQueue testing" << endl;
-
+  cout << "...end VQueue testing." << endl;
+  end = clock();
+  elapsed = end - start;
+  seconds = (double) elapsed/CLOCKS_PER_SEC;
+  cout << "elapsed time: " << seconds << " seconds " << endl;
+ 
   return 0;
 }
