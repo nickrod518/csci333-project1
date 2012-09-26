af62ff10e11f7b38855373d06cd6b747aeb0ee98
Nick Rodriguez <nick@gmail.com>
2012-09-26 15:50:12 -0400

    messed with timing

--- src/queue_tester.cpp
+++ src/queue_tester.cpp
@@ -11,31 +11,19 @@ int main() {
   int start, end, elapsed;
   double seconds;
 
-
+/*
   // AQueue testing
   start = clock();
   cout << "Begin AQueue testing..." << endl;
 
   AQueue* aQueue = new AQueue();
-  for (int i = 0; i < 10000; ++i)
-    aQueue->enqueue(i);
-  cout << "Size after enqueueing 10000: " << aQueue->size() << endl;
+  for (int j = 0; j < 10000; ++j) {
+    for (int i = 0; i < 10000; ++i)
+      aQueue->enqueue(i);
 
-  for (int i = 0; i < 5000; ++i)
-    aQueue->dequeue();
-  cout << "Size after dequeueing 5000: " << aQueue->size() << endl;
-  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
-
-  for (int i = 0; i < 2000; ++i)
-    aQueue->enqueue(i);
-  cout << "Size after enqueueing 2000 more: " << aQueue->size() << endl;
-  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
-
-  for (int i = 0; i < 7000; ++i)
-    aQueue->dequeue();
-  cout << "Size after dequeueing the remaining 7000: " << aQueue->size() << endl;
-  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
-
+    for (int i = 0; i < 10000; ++i)
+      aQueue->dequeue();
+  }
   delete aQueue;
   
   cout << "...end AQueue testing." << endl;
@@ -44,31 +32,22 @@ int main() {
   seconds = (double) elapsed/CLOCKS_PER_SEC;
   cout << "elapsed time: " << seconds << " seconds " << endl;
   cout << endl;
-
+*/
 
   // LQueue testing
   start = clock();
   cout << "Begin LQueue testing..." << endl;
 
   LQueue lQueue = LQueue();
-  for (int i = 0; i < 10000; ++i)
-    lQueue.enqueue(i);
-  cout << "Size after enqueueing 10000: " << lQueue.size() << endl;
+  cout << "got here" << endl;
+  for (int j = 0; j < 100; ++j) {
+  cout << "big loop" << j << endl;
+    for (int i = 0; i < 10000; ++i)
+      lQueue.enqueue(i);
 
-  for (int i = 0; i < 5000; ++i)
-    lQueue.dequeue();
-  cout << "Size after dequeueing 5000: " << lQueue.size() << endl;
-  cout << "Is it empty yet? " << lQueue.isEmpty() << endl;
-
-  for (int i = 0; i < 2000; ++i)
-    lQueue.enqueue(i);
-  cout << "Size after enqueueing 2000 more: " << lQueue.size() << endl;
-  cout << "Is it empty yet? " << lQueue.isEmpty() << endl;
-
-  for (int i = 0; i < 7000; ++i)
-    lQueue.dequeue();
-  cout << "Size after dequeueing the remaining 7000: " << lQueue->size() << endl;
-  cout << "Is it empty yet? " << aQueue->isEmpty() << endl;
+    for (int i = 0; i < 10000; ++i)
+      lQueue.dequeue();
+  }
 
   cout << "...end LQueue testing." << endl;
   end = clock();
@@ -83,27 +62,15 @@ int main() {
   cout << "Begin VQueue testing..." << endl;
 
   VQueue* vQueue = new VQueue();
-  for (int i = 0; i < 10000; ++i)
-    vQueue->enqueue(i);
-  cout << "Size after enqueueing 10000: " << vQueue->size() << endl;
+  for (int j = 0; j < 10000; ++j) {
+    for (int i = 0; i < 10000; ++i)
+      vQueue->enqueue(i);
 
-  for (int i = 0; i < 5000; ++i)
-    vQueue->dequeue();
-  cout << "Size after dequeueing 5000: " << vQueue->size() << endl;
-  cout << "Is it empty yet? " << vQueue->isEmpty() << endl;
-
-  for (int i = 0; i < 2000; ++i)
-    vQueue->enqueue(i);
-  cout << "Size after enqueueing 2000 more: " << vQueue->size() << endl;
-  cout << "Is it empty yet? " << vQueue->isEmpty() << endl;
-
-  for (int i = 0; i < 7000; ++i)
-    vQueue->dequeue();
-  cout << "Size after dequeueing the remaining 7000: " << vQueue->size() << endl;
-  cout << "Is it empty yet? " << vQueue->isEmpty() << endl;
-
+    for (int i = 0; i < 10000; ++i)
+      vQueue->dequeue();
+  }
   delete vQueue;
-  
+
   cout << "...end VQueue testing." << endl;
   end = clock();
   elapsed = end - start;
